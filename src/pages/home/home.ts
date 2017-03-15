4f381c009e5a38621304e8e99016279d4bc1fac6
megan.eager <megan.eager@nexusalpha.com>
2017-03-15 13:13:14 +0000

    adding list and button to home page/adding user profile button

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,9 +7,17 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+    items: any[];
 
   constructor(public navCtrl: NavController) {
-
+        this.items = [];  //will eventually be a list of events from user DB
+        for(let i = 0; i<10; i++){
+            this.items.push({
+                title: 'Event '+ i,
+                thumbNail: '../../resources/icon.png',
+                description: 'This is just one of your events',
+                id: 'eventID' + i
+            });
+        }
   }
-
 }
